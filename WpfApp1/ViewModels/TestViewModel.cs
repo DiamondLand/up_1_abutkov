29086e6f53426265061944e10d8867424fbdc7e4
Diamond Land <81316675+DiamondLand@users.noreply.github.com>
2025-01-24 10:49:57 +0300

    testresult

--- WpfApp1/ViewModels/TestViewModel.cs
+++ WpfApp1/ViewModels/TestViewModel.cs
@@ -1,8 +1,8 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.Windows.Input;
-using System;
-using WpfApp1.Helpers;
 using WpfApp1.Models;
+using WpfApp1.Helpers;
 using WpfApp1.ViewModels;
 using WpfApp1.Views;
 
@@ -10,15 +10,13 @@ public class TestViewModel : BaseViewModel
 {
     private readonly ObservableCollection<Question> _questions;
     private int _currentIndex;
-    private int _correctAnswers;
     private string _selectedAnswer;
+    private int _correctAnswers;
     private readonly string _studentId;
     private readonly string _studentName;
-    private DateTime _testStartTime;
+    private readonly DateTime _testStartTime;
 
-    public ObservableCollection<TestResult> TestResults { get; } = new ObservableCollection<TestResult>();
-
-    public Question CurrentQuestion { get; private set; }
+    public Question CurrentQuestion => _questions[_currentIndex];
     public string SelectedAnswer
     {
         get => _selectedAnswer;
@@ -26,7 +24,6 @@ public class TestViewModel : BaseViewModel
     }
 
     public ICommand NextQuestionCommand { get; }
-    public ICommand PreviousQuestionCommand { get; }
     public ICommand FinishTestCommand { get; }
 
     public TestViewModel(ObservableCollection<Question> questions, string studentId, string studentName)
@@ -36,9 +33,7 @@ public class TestViewModel : BaseViewModel
         _studentName = studentName;
         _testStartTime = DateTime.Now;
 
-        CurrentQuestion = _questions[_currentIndex];
         NextQuestionCommand = new RelayCommand(NextQuestion, CanMoveNext);
-        PreviousQuestionCommand = new RelayCommand(PreviousQuestion, CanMovePrevious);
         FinishTestCommand = new RelayCommand(FinishTest);
     }
 
@@ -49,62 +44,42 @@ public class TestViewModel : BaseViewModel
 
         if (_currentIndex < _questions.Count)
         {
-            CurrentQuestion = _questions[_currentIndex];
             SelectedAnswer = null;
             OnPropertyChanged(nameof(CurrentQuestion));
-            OnPropertyChanged(nameof(SelectedAnswer));
         }
     }
 
-    private void PreviousQuestion()
+    private void SaveAnswer()
     {
-        _currentIndex--;
-        if (_currentIndex >= 0)
+        if (SelectedAnswer == CurrentQuestion.CorrectAnswer)
         {
-            CurrentQuestion = _questions[_currentIndex];
-            OnPropertyChanged(nameof(CurrentQuestion));
+            _correctAnswers++;
         }
     }
 
     private bool CanMoveNext() => _currentIndex < _questions.Count - 1;
-    private bool CanMovePrevious() => _currentIndex > 0;
-
-    private void SaveAnswer()
-    {
-        if (!string.IsNullOrEmpty(SelectedAnswer))
-        {
-            if (SelectedAnswer == CurrentQuestion.CorrectAnswer)
-            {
-                _correctAnswers++;
-            }
-        }
-    }
-
 
     private void FinishTest()
     {
         SaveAnswer();
 
-        int totalQuestions = _questions.Count;
-        int duration = (int)(DateTime.Now - _testStartTime).TotalMinutes;
-        string grade = _correctAnswers >= totalQuestions * 0.8 ? "Отлично" :
-                       _correctAnswers >= totalQuestions * 0.6 ? "Хорошо" : "Удовлетворительно";
+        var totalQuestions = _questions.Count;
+        var durationMinutes = (int)(DateTime.Now - _testStartTime).TotalMinutes;
+        var grade = _correctAnswers >= totalQuestions * 0.8 ? "Отлично" :
+                    _correctAnswers >= totalQuestions * 0.6 ? "Хорошо" : "Удовлетворительно";
 
-        var testResult = new TestResult
+        var result = new TestResult
         {
-            TestId = 1,
             StudentId = _studentId,
             StudentName = _studentName,
             TestDate = _testStartTime,
             TotalQuestions = totalQuestions,
             CorrectAnswers = _correctAnswers,
             Grade = grade,
-            DurationMinutes = duration
+            DurationMinutes = durationMinutes
         };
 
-        TestResults.Add(testResult);
-
-        ShowResults(testResult);
+        ShowResults(result);
     }
 
     private void ShowResults(TestResult result)
